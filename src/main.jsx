090ee9895271d469e1aa7a5b3f3b9827a95df7f2
Kong_Sisovandara <sisovandara@gmail.com>
2025-02-19 23:42:03 +0700

    Folder Structure and Config

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import AboutUsPage from "./pages/AboutUsPage.jsx";
+import RootLayout from "./components/layouts/RootLayout.jsx";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/pages/AboutUsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutUsPage() {
+  return (
+    <>
+      <h1>AboutUs page</h1>
+    </>
+  )
+}
