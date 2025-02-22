e03c89342154ca9c3d3244b60a83346f994e21d0
kea-daron <daron3327@gmail.com>
2025-02-22 23:21:37 +0700

    FooterComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/layouts/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-MainTitle underline text-accent'>Hello world</h1>
+      <Footer/>
     </>
   )
 }
--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -1,11 +1,9 @@
 import React from 'react' 
-import Navbar from './Navbar'
 import { Outlet } from 'react-router'
 
 export default function RootLayout() {
   return (
     <div>
-      <Navbar/>
       <Outlet/>
     </div>
   )
